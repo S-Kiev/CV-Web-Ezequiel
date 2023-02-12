226eeff14da908b29de976ba64f80dccf8114e18
S-Kiev <vezequiel58@gmail.com>
2023-02-12 10:57:40 -0300

    menu2

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ function mostrarOcultarMenu(){
         menu_visible = false;
     }
 }
+
+//se oculta el menu una vez que seleccionada una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
